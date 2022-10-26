04cf3941948c6cb89dd2a1525c870103d2580c19
Axent13 <Axent13@yandex.ru>
2022-10-26 12:08:41 +0700

    Created component promoSlider

--- src/layouts/mainpage.jsx
+++ src/layouts/mainpage.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import PromoSlider from '../components/promoSlider';
+
+import './mainpage.scss';
 
 const Mainpage = () => {
   return (
     <>
       <Header />
-      <h2>Mainpage content</h2>
+      {/* Mainpage content */}
+      <div className='mainpage__promo-slider'>
+        <PromoSlider />
+      </div>
       <Footer />
     </>
   );
