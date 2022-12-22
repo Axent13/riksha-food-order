2d05aa62c32371132475b713675d1f02807f7b76
Axent13 <axent13@yandex.ru>
2022-12-22 23:33:15 +0700

    created SectionHeader

--- src/layouts/mainpage.jsx
+++ src/layouts/mainpage.jsx
@@ -5,6 +5,12 @@ import PromoSlider from '../components/promoSlider';
 
 import './mainpage.scss';
 import FeaturesList from '../components/Features/featuresList';
+import topRollImg from '../assets/top-roll.png';
+import hugRollsImg from '../assets/hug-rolls.png';
+import happyRollImg from '../assets/happy-roll.png';
+import tableRollImg from '../assets/table-roll.png';
+import messageRollImg from '../assets/message-roll.png';
+import SectionHeader from '../components/sectionHeader';
 
 const Mainpage = () => {
   return (
@@ -14,6 +20,15 @@ const Mainpage = () => {
       <div className='mainpage__promo-slider'>
         <PromoSlider />
         <FeaturesList />
+        <SectionHeader
+          text='Топ позиции'
+          subText='Новинки'
+          leftTopImg={topRollImg}
+        />
+        <SectionHeader text='Комбо меню' rightCenterImg={hugRollsImg} />
+        <SectionHeader text='Акции' rightCenterImg={happyRollImg} />
+        <SectionHeader text='Категории' rightCenterImg={tableRollImg} />
+        <SectionHeader text='О компании' rightCenterImg={messageRollImg} />
       </div>
       <Footer />
     </>
