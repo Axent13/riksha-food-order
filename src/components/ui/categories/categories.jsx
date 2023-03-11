ac075bc638258fe383af1ee6feb638a40fe65802
Axent13 <Axent13@yandex.ru>
2023-03-11 16:06:13 +0700

    Сделал вывод продуктов в Categories

--- src/components/ui/categories/categories.jsx
+++ src/components/ui/categories/categories.jsx
@@ -1,12 +1,76 @@
 import React from 'react';
 import FoodCategoryButtonList from '../../common/foodCategory/foodCategoryButtonList';
+import ProductCard from '../productCard/productCard';
+
+import rollPhiladelphiaImg from '../../../assets/productsImages/roll-philadelphia.png';
+import rollCreamyShrimpImg from '../../../assets/productsImages/roll-creamy-shrimp.png';
 
 import './categories.scss';
 
+const products = [
+  {
+    _id: 'd57ac044-a8fe-11ed-afa1-0242ac120002',
+    title: 'Ролл "Филадельфия"',
+    imageSrc: rollPhiladelphiaImg,
+    components: 'Лосось, сыр "Филадельфия", огурец, авокадо',
+    properties: {
+      isHotRoll: true,
+      isSpicy: true,
+    },
+    features: {
+      isTop: true,
+    },
+    weight: '200 грамм',
+    kkals: '130 Ккал',
+    oldPrice: '269 ₽',
+    newPrice: '219 ₽',
+  },
+  {
+    _id: 'd57ac4d6-a8fe-11ed-afa1-0242ac120002',
+    title: 'Ролл "Сливочная креветка"',
+    imageSrc: rollCreamyShrimpImg,
+    components: 'Лосось, сыр "Филадельфия", огурец, авокадо',
+    properties: {
+      isHotRoll: true,
+    },
+    features: {
+      isTop: true,
+    },
+    weight: '200 грамм',
+    kkals: '130 Ккал',
+    oldPrice: '224 ₽',
+    newPrice: '219 ₽',
+  },
+  {
+    _id: 'd57ac6fc-a8fe-11ed-afa1-0242ac120002',
+    title: 'Ролл "Чикен энд Чиз"',
+    imageSrc: rollPhiladelphiaImg,
+    components: 'Лосось, сыр "Филадельфия", огурец, авокадо',
+    properties: {
+      isHotRoll: true,
+      isSpicy: true,
+    },
+    features: {
+      isTop: true,
+    },
+    weight: '200 грамм',
+    kkals: '130 Ккал',
+    oldPrice: '232 ₽',
+    newPrice: '199 ₽',
+  },
+];
+
 const Categories = () => {
   return (
-    <div className='categories__categories-buttons'>
-      <FoodCategoryButtonList />
+    <div className='categories'>
+      <div className='categories__categories-buttons'>
+        <FoodCategoryButtonList />
+      </div>
+      <div className='categories__products'>
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
