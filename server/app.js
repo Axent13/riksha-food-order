f1330f609d005d7acba40789041b8cffa984b39c
Axent13 <Axent13@yandex.ru>
2023-03-17 14:27:02 +0700

    Получение списка всех продуктов по API

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.use("/api", routes);
+app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
