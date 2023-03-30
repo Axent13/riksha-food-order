c2ba73cc6ba5434ee5600a30d8f78a38f3b36d1c
Axent13 <Axent13@yandex.ru>
2023-03-30 12:00:19 +0700

    fix names

---
+++ client/src/components/common/backToCatalogButton/backToCatalogButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './backToCatalogButton.scss';
+
+const BackToCatalogButton = ({ categoryName }) => {
+  return (
+    <Link className='back-to-catalog-button' to={`/category/${categoryName}`}>
+      <div className='back-to-catalog-button__arrow'></div>
+      <div className='back-to-catalog-button__text'>Назад в каталог</div>
+    </Link>
+  );
+};
+
+BackToCatalogButton.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+};
+
+export default BackToCatalogButton;
