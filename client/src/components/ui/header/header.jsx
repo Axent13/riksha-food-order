b605044cb4198f79e17b988348b0da9fb57099e5
Axent13 <Axent13@yandex.ru>
2023-05-15 15:50:36 +0700

    Mainpage adaptive

--- client/src/components/ui/header/header.jsx
+++ client/src/components/ui/header/header.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../../common/cart/cart';
-import Logo from '../../logo';
 import Navbar from '../../common/navbar/navbar';
 import PhoneNumber from '../../common/phoneNumber/phoneNumber';
 import Profile from '../../common/profile/profile';
 import './header.scss';
 import { foodCategoryInfo } from '../../common/foodCategory/foodCategoryInfo';
 import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../logo/logo';
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const [isBurgerMenuShown, setIsBurgerMenuShown] = useState(false);
 
   return (
     <header className='header'>
@@ -21,6 +23,33 @@ const Header = () => {
         </div>
         <div className='header__up-part'>
           <Navbar />
+          <div className='header__burger'>
+            <button
+              className={
+                'header__burger-button' +
+                (isBurgerMenuShown ? ' header__burger-button_opened' : '')
+              }
+              onClick={() => setIsBurgerMenuShown((prevState) => !prevState)}
+            ></button>
+            {isBurgerMenuShown && (
+              <div className='header__burger-menu'>
+                {foodCategoryInfo.map((category, index) => (
+                  <button
+                    key={index}
+                    className='header__button'
+                    onClick={() => {
+                      navigate(`/category/${category.categoryName}`, {
+                        replace: true,
+                      });
+                    }}
+                  >
+                    <img src={category.image} alt={category.text} />
+                    <span className='header__button-text'>{category.text}</span>
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
           <PhoneNumber />
           <Link to='/cart'>
             <Cart />
